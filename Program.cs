2479d5373af087dd6db0e6fd260f39df154ed408
LemonMeringuePie <mbelopashencev96@gmail.com>
2022-12-01 21:58:32 +0500

    программа, выполняющая деление и округление двух чисел

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testMetod2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number one");
+            double numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number one");
+            double numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine(divideAndRound(numberOne, numberTwo));
+            Console.ReadLine();
+        }
+        static int divideAndRound(double a, double b)
+        {
+            return (int)Math.Round(a / b);
+        }
+    }
+}
